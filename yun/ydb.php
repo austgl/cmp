59ced096c7be7f4b8da282edf4698acad3e0e7ea
ganlei <769323213@qq.com>
2013-02-04 21:31:54 +0800

    second
    second

---
+++ yun/ydb.php
@@ -0,0 +1,42 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>龙腾影视Tv云点播(tv.ltqq.net)</title>
+<link href="js/vod.css" rel="stylesheet" type="text/css"/>
+<meta name="keywords" content=""/>
+<meta name="description" content=""/>
+</head>
+<body>
+<div class="container">
+	<div class="container" id="header">
+		<div class="w960">
+	<h1><a href="//tv.ltqq.net/" title="龙腾云点播(tv.ltqq.net)">龙腾云点播(tv.ltqq.net)</a></h1>
+		</div>
+	<hr/>
+	</div>
+	<div class="w960" id="video">
+		<div id="player-head">正在播放：<span id="filename"></span></div>
+		<div id="player"></div>
+		<div id="player-foot"></div>
+	</div>
+	<div class="w960" id="operate">
+		<div id="operate-input"><form onsubmit="javascript:x2.cleanUrl();return false;"><input id="u" type="text" name="u" value="" placeholder="输入视频下载地址"/><a id="play-button" class="btn" href="javascript://">播放视频</a><a id="upload-button" class="btn" href="javascript://">上传种子</a></form></div>
+	</div>
+        <div class="w960" id="footer"><hr/>&copy; 2012 - 2013 Tv.LtQq.Net</div>
+
+
+</div>
+<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="js/swfobject.js"></script>
+<script type="text/javascript" src="js/yun.js"></script>
+<script type="text/javascript" src="http://api.naliso.com/tj.js"></script>
+</body>
+</html>
+<!-- Baidu Button BEGIN -->
+<script type="text/javascript" id="bdshare_js" data="type=slide&amp;img=5&amp;pos=right&amp;uid=6465151" ></script>
+<script type="text/javascript" id="bdshell_js"></script>
+<script type="text/javascript">
+document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + Math.ceil(new Date()/3600000);
+</script>
+<!-- Baidu Button END -->
